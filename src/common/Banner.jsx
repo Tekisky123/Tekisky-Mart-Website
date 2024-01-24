113d280db5e6adee5162bba820bef37b54f23018
Tekisky123 <153596067+Tekisky123@users.noreply.github.com>
2024-01-24 16:11:33 +0530

    hello

---
+++ src/common/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Banner = ({ banners }) => {
+  return (
+    <div className="banner">
+      <div className="container">
+        <div className="slider-container has-scrollbar">
+          {banners.map((banner, index) => (
+            <div className="slider-item" key={index}>
+              <img src={banner.imgSrc} alt={banner.subtitle} className="banner-img" />
+              <div className="banner-content">
+                <p className="banner-subtitle">{banner.subtitle}</p>
+                <h2 className="banner-title">{banner.title}</h2>
+                <p className="banner-text">
+                  starting at &dollar; <b>{banner.price}</b>
+                </p>
+                <Link to={banner.link} className="banner-btn">
+                  Shop now
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
