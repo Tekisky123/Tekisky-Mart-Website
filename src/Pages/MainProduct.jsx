6b69154e3ebee000015bde8af424cbf26b9b2f34
sufiyan <suifyan.patel.pr.7@gmail.com>
2024-01-24 16:11:49 +0530

    jbdjvb

--- src/Pages/MainProduct.jsx
+++ src/Pages/MainProduct.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductShowcase from '../common/ProductShowCase';
 // import ProductShowcase from './ProductShowcase'; // Make sure to adjust the path
 
 const ProductMain = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://tekiskymart.onrender.com/admin/getproduct');
+        setProducts(response.data.products);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <div className="product-main">
       <h2 className="title">New Products</h2>
 
       <div className="product-grid">
-        {/* Product 1 */}
-        <ProductShowcase
-          category="jacket"
-          title="Mens Winter Leathers Jackets"
-          imgSrcDefault="./assets/images/products/jacket-3.jpg"
-          imgSrcHover="./assets/images/products/jacket-4.jpg"
-          badge={{ text: '15%', className: '' }}
-          price="48.00"
-          originalPrice="75.00"
-          rating={{ fullStars: 3, emptyStars: 2 }}
-        />
-        {/* Add more ProductShowcase components for other products */}
+
+           {products.map((product) => (
+          <ProductShowcase
+            key={product._id}
+            category={product.productCategory}
+            title={product.productName}
+            imgSrcDefault={product.imageURL[0]}
+            imgSrcHover={product.imageURL[1]||product.imageURL[0]}
+            badge={{ text: '15%', className: '' }}
+            price={product.productDetails[0].offerPrice}
+            originalPrice={product.productDetails[0].mrp}
+            rating={{ fullStars: 3, emptyStars: 2 }}
+          />
+        ))}
+
       </div>
     </div>
   );
