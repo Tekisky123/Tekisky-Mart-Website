73877a2b0fe04bde7729a62cfc5f579cd0e937a6
Tekisky123 <153596067+Tekisky123@users.noreply.github.com>
2024-01-24 15:01:39 +0530

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Header/>
+      </BrowserRouter>
     </div>
   );
 }
